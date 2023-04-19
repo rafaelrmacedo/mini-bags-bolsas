4c8c8270289076813f9190205e2d63d9cdc41f78
rafaelrodriguesmacedo2004@gmail.com <rafaelrodriguesmacedo2004@gmail.com>
2023-04-19 16:02:54 -0300

    dependency added

---
+++ backend/minibags-api/src/main/java/minibags/minibagsapi/model/ProductModel.java
@@ -0,0 +1,16 @@
+package minibags.minibagsapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class ProductModel {
+
+    @Id
+
+
+}
